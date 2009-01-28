62795bd64481aca91abf7b43558c060b75bb5c40
phillip.dixon@gmail.com <none@none>
2009-01-28 18:50:59 +1300

    The big restructure.
    Shoving everything into a single .emacs file has got unmanageable.
    This restructure moves everything in to .emacs.d. init.el is now the
    entry point. Externally maintained code has been moved to vendor dir.
    
    --HG--
    rename : .emacs => tweaks/.emacs
    rename : anything-config.el => vendor/anything-config.el
    rename : anything-make.el => vendor/anything-make.el
    rename : anything.el => vendor/anything.el
    rename : color-theme-autoloads.el => vendor/color-theme-autoloads.el
    rename : color-theme.el => vendor/color-theme.el
    rename : ergonomic_keybinding_dvorak.el => vendor/ergonomic_keybinding_dvorak.el
    rename : find-cmd.el => vendor/find-cmd.el
    rename : js2.el => vendor/js2.el
    rename : mercurial.el => vendor/mercurial.el
    rename : outline-magic.el => vendor/outline-magic.el
    rename : pair-mode.el => vendor/pair-mode.el
    rename : project-root.el => vendor/project-root.el
    rename : taskpaper.el => vendor/taskpaper.el
    rename : themes/color-theme-example.el => vendor/themes/color-theme-example.el
    rename : themes/color-theme-library.el => vendor/themes/color-theme-library.el
    rename : xgtags.el => vendor/xgtags.el
    rename : yasnippet.el => vendor/yasnippet.el
    rename : zenburn.el => vendor/zenburn.el

---
+++ init.el
@@ -0,0 +1,22 @@
+(setq dotfiles-dir (file-name-directory
+		    (or (buffer-file-name) load-file-name)))
+
+(add-to-list 'load-path dotfiles-dir)
+(add-to-list 'load-path (concat dotfiles-dir "/vendor"))
+
+(setq custom-file (concat dotfiles-dir "custom.el"))
+
+;; Load up my config stuff
+(setq tweaks-dir (concat dotfiles-dir "/tweaks"))
+(mapc #'load (directory-files tweaks-dir nil ".*el$"))
+
+;; load the customize stuff
+(load custom-file 'noerror)
+
+;; Work around a bug on OS X where system-name is FQDN
+(if (eq system-type 'darwin)
+    (setq system-name (car (split-string system-name "\\."))))
+
+;; You can keep system-specific customizations here
+(setq system-specific-config (concat dotfiles-dir system-name ".el"))
+(if (file-exists-p system-specific-config) (load system-specific-config))
