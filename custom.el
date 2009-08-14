654bc95636280d479464a32ed16a1b9c4ca561fb
Phillip DIXON <phil@dixon.gen.nz>
2009-08-14 16:25:51 +1200

    Objective C tweaks

--- custom.el
+++ custom.el
@@ -4,6 +4,7 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(column-number-mode t)
+ '(ns-pop-up-frames nil)
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil))
--- tweaks/30-defuns.el
+++ tweaks/30-defuns.el
@@ -97,3 +97,26 @@
   (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
     (when file
       (find-file file))))
+
+(defun bh-choose-header-mode ()
+  (interactive)
+  (if (string-equal (substring (buffer-file-name) -2) ".h")
+      (progn
+        ;; OK, we got a .h file, if a .m file exists we'll assume it's
+        ; an objective c file. Otherwise, we'll look for a .cpp file.
+        (let ((dot-m-file (concat (substring (buffer-file-name) 0 -1) "m"))
+              (dot-cpp-file (concat (substring (buffer-file-name) 0 -1) "cpp")))
+          (if (file-exists-p dot-m-file)
+              (progn
+                (objc-mode)
+                )
+            (if (file-exists-p dot-cpp-file)
+                (c++-mode)
+              )
+            )
+          )
+        )
+    )
+  )
+
+(add-hook 'find-file-hook 'bh-choose-header-mode)
