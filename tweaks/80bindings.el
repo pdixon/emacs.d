2df9ce5e0e809a8f45138221b6dc959b7edca95b
Phillip DIXON <pdixon@dynamiccontrols.com>
2009-12-14 16:27:22 +1300

    Advise C-w and M-w so they operate on the current line if nothing is selected.

--- tweaks/80bindings.el
+++ tweaks/80bindings.el
@@ -234,3 +234,17 @@ transpose line.
  (cond ((bolp) (transpose-lines arg))
        ((looking-at "[[:space:]]") (transpose-words arg))
        (t (transpose-chars arg))))
+
+;;;; And some other bits and pieces.
+(defadvice kill-ring-save (before slick-copy activate compile) "When called
+  interactively with no active region, copy a single line instead."
+  (interactive (if mark-active (list (region-beginning) (region-end)) (message
+  "Copied line") (list (line-beginning-position) (line-beginning-position
+  2)))))
+
+(defadvice kill-region (before slick-cut activate compile)
+  "When called interactively with no active region, kill a single line instead."
+  (interactive
+    (if mark-active (list (region-beginning) (region-end))
+      (list (line-beginning-position)
+        (line-beginning-position 2)))))
