8a209020ab5f1eca1b9b5fe5ebb4a85b2be5f759
Phillip DIXON <pdixon@dynamiccontrols.com>
2010-01-14 10:54:16 +1300

    Tie org mode into appt

--- tweaks/50org-mode.el
+++ tweaks/50org-mode.el
@@ -135,3 +135,21 @@
                (setq yas/fallback-behavior
                      '(apply ,original-command))
                (local-set-key [tab] 'yas/expand))))
+
+; Erase all reminders and rebuilt reminders for today from the agenda
+(defun bh/org-agenda-to-appt ()
+  (interactive)
+  (setq appt-time-msg-list nil)
+  (org-agenda-to-appt))
+
+; Rebuild the reminders everytime the agenda is displayed
+(add-hook 'org-finalize-agenda-hook 'bh/org-agenda-to-appt)
+
+; This is at the end of my .emacs - so appointments are set up when Emacs starts
+(bh/org-agenda-to-appt)
+
+; Activate appointments so we get notifications
+(appt-activate t)
+
+; If we leave Emacs running overnight - reset the appointments one minute after midnight
+(run-at-time "24:01" nil 'bh/org-agenda-to-appt)
