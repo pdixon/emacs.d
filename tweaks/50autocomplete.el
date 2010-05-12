267ce6ea766821b14aeb69c4a627984064279f16
Phillip DIXON <phil@dixon.gen.nz>
2010-05-12 23:01:16 +1200

    Fancy autocomplete for elisp editting.

--- tweaks/50autocomplete.el
+++ tweaks/50autocomplete.el
@@ -1,3 +1,29 @@
+;;; 50autocomplete.el --- Setup for autocomplete
+
+;; Copyright (C) 2010  Phillip Dixon
+
+;; Author: Phillip Dixon <phil@dixon.gen.nz>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
 (require 'auto-complete)
 (require 'auto-complete-config)
 (global-auto-complete-mode t)
@@ -5,7 +31,18 @@
 (setq ac-dwim t)
 (setq ac-override-local-map nil)
 
-(setq-default ac-sources 
-              '(ac-source-yasnippet 
-                ac-source-abbrev 
+(setq-default ac-sources
+              '(ac-source-yasnippet
+                ac-source-abbrev
                 ac-source-words-in-buffer))
+
+(add-hook 'emacs-lisp-mode-hook
+          '(lambda ()
+             (add-to-list 'ac-sources 'ac-source-symbols)
+             (add-to-list 'ac-sources 'ac-source-functions)
+             (add-to-list 'ac-sources 'ac-source-features)
+             (add-to-list 'ac-sources 'ac-source-variables)))
+
+(provide '50autocomplete)
+;;; 50autocomplete.el ends here
+
