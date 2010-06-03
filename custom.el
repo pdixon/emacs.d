a46c199d89592ca21499e1576ada3d8473539470
Phillip DIXON <phil@dixon.gen.nz>
2010-06-03 20:23:12 +1200

    Leave the face selection to Xresources.

--- custom.el
+++ custom.el
@@ -14,9 +14,3 @@
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "Liberation Mono")))))
