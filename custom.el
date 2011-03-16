b9fb2ff4d9301540516ab3b93dab05fc18c78ac7
Phillip DIXON <phil@dixon.gen.nz>
2011-03-16 17:57:13 +1300

    Disable rst-modes illegible highlighting.

--- custom.el
+++ custom.el
@@ -1,8 +1,8 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(display-time-mode nil)
  '(ns-pop-up-frames nil)
@@ -10,7 +10,14 @@
  '(pr-gv-command "evince")
  '(pr-ps-printer-alist (quote ((default "gtklp" nil "" nil))))
  '(pr-txt-printer-alist (quote ((default "gtklp" nil nil))))
+ '(rst-adornment-faces-alist (quote ((t . font-lock-keyword-face) (nil . font-lock-keyword-face) (1 . markdown-header-face) (2 . markdown-header-face) (3 . markdown-header-face) (4 . markdown-header-face) (5 . markdown-header-face) (6 . markdown-header-face))))
  '(safe-local-variable-values (quote ((user-mail-address . "pdixon@dynamiccontrols.com") (user-company . "Dynamic Controls"))))
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
