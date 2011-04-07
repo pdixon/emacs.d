5180252d9128a2f28ef881e64be9ffdb5a7b1c90
Phillip DIXON <phil@dixon.gen.nz>
2011-04-07 19:59:50 +1200

    Automate elpa package installation.
    Use this to move from tracking magit repo to installing the package.

--- tweaks/10package.el
+++ tweaks/10package.el
@@ -24,9 +24,42 @@
 
 ;;; Code:
 
+(require 'cl)
+
 (require 'package)
 (add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+(defvar pd-packages (list 'magit
+                          'gist))
+
+(defun starter-kit-elpa-install ()
+  "Install all starter-kit packages that aren't installed."
+  (interactive)
+  (dolist (package pd-packages)
+    (unless (or (member package package-activated-list)
+                (functionp package))
+      (message "Installing %s" (symbol-name package))
+      (package-install package))))
+
+(defun esk-online? ()
+  "See if we're online.
+
+Windows does not have the network-interface-list function, so we
+just have to assume it's online."
+  ;; TODO how could this work on Windows?
+  (if (and (functionp 'network-interface-list)
+           (network-interface-list))
+      (some (lambda (iface) (unless (equal "lo" (car iface))
+                         (member 'up (first (last (network-interface-info
+                                                   (car iface)))))))
+            (network-interface-list))
+    t))
+
+;; On your first run, this should pull in all the base packages.
+(when (esk-online?)
+  (unless package-archive-contents (package-refresh-contents))
+  (starter-kit-elpa-install))
 
 ;;(provide '(package-support))
 ;;; (10package.el) ends here
