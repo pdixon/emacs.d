5409bb7043198a1e96b66d59530b20ebc3792a02
Phillip DIXON <phil@dixon.gen.nz>
2011-05-22 21:31:20 +1200

    Tweak the way ido operates.

--- tweaks/20-misc.el
+++ tweaks/20-misc.el
@@ -90,10 +90,13 @@
 (pr-update-menus)
 
 ;; Setup for ido.el
-(require 'ido)
-(ido-mode t)
-(setq ido-enable-flex-matching t
-      ido-use-filename-at-point t)
+(when (functionp 'ido-mode)
+  (ido-mode t)
+  (setq ido-enable-prefix nil
+        ido-enable-flex-matching t
+        ido-auto-merge-work-directories-length -1
+        ido-create-new-buffer 'always
+        ido-use-filename-at-point 'guess))
 
 (require 'solarized-light-theme)
 
