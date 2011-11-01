0e9029d5edbbddfb82f6326c1b3150960a38e91b
Phillip DIXON <phil@dixon.gen.nz>
2011-11-01 15:45:37 +1300

    Python and electric-indent-mode don't mix.

--- tweaks/50python.el
+++ tweaks/50python.el
@@ -1,17 +1,10 @@
-;; Set up Flymake to use PyFlakes.
-;; (eval-after-load "flymake"
-;;   '(progn
-;;      (defun flymake-pyflakes-init ()
-;;        (let* ((temp-file (flymake-init-create-temp-buffer-copy
-;;                           'flymake-create-temp-inplace))
-;;               (local-file (file-relative-name
-;;                            temp-file
-;;                            (file-name-directory buffer-file-name))))
-;;          (list "pyflakes" (list local-file))))
-     
-;;      (add-to-list 'flymake-allowed-file-name-masks
-;;                   '("\\.py\\'" flymake-pyflakes-init))))
 
-;; (add-hook 'python-mode-hook
-;;           (lambda ()             
-;;             (unless (eq buffer-file-name nil) (flymake-mode 1))))
+(defun pd-python-mode-hook ()
+  (electric-indent-mode -1) ;; This isn't useful in python
+  )
+
+(add-hook 'python-mode-hook 'pd-python-mode-hook)
+
+(provide 'pd-python)
+
+;;; 50python.el ends here
