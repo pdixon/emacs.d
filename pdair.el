44b78c9036ac04d6546e368c7690145cbfe06b0f
Phillip DIXON <phil@dixon.gen.nz>
2011-11-05 15:46:31 +1300

    Add a machine specific file for the air.

---
+++ pdair.el
@@ -0,0 +1,31 @@
+;;; pdair.el --- Machine specific settings for my MacBook Air
+
+;; Copyright (C) 2011  Phillip Dixon
+
+;; Author: Phillip Dixon <phil@dixon.gen.nz>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(when window-system
+  (ubuntu-mono 1))
+
+(provide 'pdair)
+;;; pdair.el ends here
