62da2fd72cd72164a480e4992726970c4d6f5c0e
Phillip DIXON <phil@dixon.gen.nz>
2011-12-04 11:21:24 +1300

    New color themes.

--- custom.el
+++ custom.el
@@ -5,8 +5,9 @@
  ;; If there is more than one, they won't work right.
  '(ac-modes (quote (emacs-lisp-mode lisp-mode lisp-interaction-mode slime-repl-mode c-mode cc-mode objc-mode c++-mode java-mode malabar-mode clojure-mode scala-mode scheme-mode ocaml-mode tuareg-mode haskell-mode perl-mode cperl-mode python-mode ruby-mode ecmascript-mode javascript-mode js-mode js2-mode php-mode css-mode makefile-mode sh-mode fortran-mode f90-mode ada-mode xml-mode sgml-mode)))
  '(column-number-mode t)
- '(custom-safe-themes (quote ("2a971f0c829dc21a40f78ca94e738ed725419c1a" "0174d99a8f1fdc506fa54403317072982656f127" "5600dc0bb4a2b72a613175da54edb4ad770105aa" "8bf936e14bdceb3271cfc96fcf687d8267f8e251" default)))
+ '(custom-safe-themes (quote ("2b1bb3493a73ddf9da5785479170a402b1c949ac" "1f392dc4316da3e648c6dc0f4aad1a87d4be556c" "1d2126921f639e9e3b9275617555729583578ded" "2a971f0c829dc21a40f78ca94e738ed725419c1a" "0174d99a8f1fdc506fa54403317072982656f127" "5600dc0bb4a2b72a613175da54edb4ad770105aa" "8bf936e14bdceb3271cfc96fcf687d8267f8e251" default)))
  '(display-time-mode nil)
+ '(indent-tabs-mode nil)
  '(ns-pop-up-frames nil)
  '(org-modules (quote (org-bbdb org-bibtex org-gnus org-info org-jsinfo org-habit org-irc org-mew org-mhe org-protocol org-rmail org-vm org-wl org-w3m)))
  '(pr-gv-command "evince")
