84d38820d2b5346c13fa850372d8af0a6ef7a599
Phillip DIXON <phil@dixon.gen.nz>
2012-01-31 15:59:09 +1300

    Dump old xcode support.

--- tweaks/50cc-mode.el
+++ tweaks/50cc-mode.el
@@ -20,50 +20,6 @@
 
 (add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
 
-;; Xcode support from http://han9kin.doesntexist.com/29
-(defvar xcode-compile-sdks nil)
-(defvar xcode-compile-sdk-history nil)
-
-(defun my-objc-hook ()
-  (dolist (line
-           (split-string
-            (with-temp-buffer
-              (call-process "xcodebuild" nil t nil "-showsdks")
-              (buffer-string))
-            "\n" t))
-    (let ((comps (split-string line "-sdk " t)))
-      (if (> (length comps) 1)
-          (add-to-list 'xcode-compile-sdks (car (last comps)))))))
-
-;(add-hook `objc-mode-hook `my-objc-hook)
-
-(defun xcode-compile ()
-  (interactive)
-  (let ((command "xcodebuild -activeconfiguration -activetarget"))
-    (setq command
-          (concat
-           command
-           (if xcode-compile-sdks
-               (let ((default-sdk (or (car xcode-compile-sdk-history) (car xcode-compile-sdks))))
-                 (concat
-                  " -sdk "
-                  (completing-read
-                   (format "Compile with sdk (default %s): " default-sdk)
-                   xcode-compile-sdks
-                   nil
-                   t
-                   nil
-                   'xcode-compile-sdk-history
-                   default-sdk))))
-           (let ((dir ".") (files nil))
-             (while
-                 (progn
-                   (setq files (directory-files dir nil "\\.xcodeproj\\'"))
-                   (and (not (string-equal "/" (expand-file-name dir))) (null files)))
-               (setq dir (concat (file-name-as-directory dir) "..")))
-             (unless (null files) (concat " -project " (file-name-as-directory dir) (car files))))))
-    (compile (read-string "Compile command: " (concat command " ")))))
-
 ;; Compilation mode stuff
 (defun pd/compilation-hook ()
   (setq truncate-lines t))
