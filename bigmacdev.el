c61be1a7462061e18c8f4bbabc4c0305ed83162f
Phillip DIXON <phil@dixon.gen.nz>
2012-04-11 15:37:33 +1200

    Local machine updates.

--- bigmacdev.el
+++ bigmacdev.el
@@ -3,10 +3,10 @@
 (setq ispell-program-name "/opt/homebrew/bin/aspell")
 
 (setq org-latex-to-pdf-process
-  '("/usr/texbin/pdflatex -interaction nonstopmode %s"
-    "/usr/texbin/pdflatex -interaction nonstopmode %s"))
+      '("/usr/texbin/pdflatex -interaction nonstopmode %s"
+        "/usr/texbin/pdflatex -interaction nonstopmode %s"))
 
-(dir-locals-set-class-variables 
+(dir-locals-set-class-variables
  'work-directory
  '((nil . ((user-company . "Dynamic Controls")
            (user-mail-address . "pdixon@dynamiccontrols.com")))))
@@ -18,8 +18,13 @@
       '(("iChair Trac" "http://ichair.sw/report/7?format=rss&USER=pdixon"
          "~/org/inbox.org" "iChair Assigned Issues")))
 
-(setq url-proxy-services '(("no_proxy" . "\\.sw")
-			   ("http" . "127.0.0.1:3128")))
+(setq url-proxy-services '(("no_proxy" . "\\.au.ivc")
+                           ("http" . "127.0.0.1:3128")))
+(require 'mediawiki)
+(add-to-list 'mediawiki-site-alist
+             '("Software" "http://wiki.sw.au.ivc/mediawiki" "pdixon" "" "The PENSIEVE"))
+
+(setq mediawiki-site-default "Software")
 
 (when window-system
   (ubuntu-mono 1))
