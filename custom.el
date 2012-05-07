57d61ed4ad682fab6d8b7c211d3b78fbf41faf7d
Phillip DIXON <phil@dixon.gen.nz>
2012-05-07 22:19:33 +1200

    Remove hard-code face overrides.

--- custom.el
+++ custom.el
@@ -17,9 +17,3 @@
  '(scss-compile-at-save nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(cursor ((t (:background "#3f3f3f" :inverse-video t)))))
