c585c45247469150eb757bfe24987961a4e84bd9
Phillip DIXON <phil@dixon.gen.nz>
2012-05-19 22:21:03 +1200

    functions for switch between related obj-c files.
    pd/toggle-header switches between the header and the implementation (and
    vice versa).
    
    pd/toggle-test switches between the implementation and the unit
    test (and again vice versa).

--- tweaks/50cc-mode.el
+++ tweaks/50cc-mode.el
@@ -32,22 +32,53 @@
 
 (add-hook 'objc-mode-hook 'pd/objc-ff-setup-hook)
 
-(defun pd/toggle-header ()
+(defun pd/toggle-header (&optional in-other-window)
   ""
-  (interactive)
-  (ff-find-other-file nil t))
+  (interactive "P")
+  (ff-get-other-file in-other-window))
 
-(defun pd/xcode-target-dirs ()
-  (if ((= (substring default-directory -6 -1) "Tests"))
-      ((list "." (substring default-director 0 -6)))
-    ((list "." (concat (substring default-directory 0 -1) "Tests")))))
-
-(defun pd/toggle-test ()
+(defun pd/toggle-test (&optional in-other-window)
   ""
-  (interactive)
-  (let
-      ((ff-other-file-alist '((".m" ("Tests.m"))
-                              ("Tests.m" (".m"))))
-       (ff-always-try-to-create nil)
-       (ff-search-directories (pd/xcode-target-dirs)))
-    (ff-find-other-file nil t)))
+  ;; This is based on ff-find-the-other-file, but without all the
+  ;; customizable variables
+  (interactive "P")
+  (let (match     ;; matching regexp for this file.
+        pos       ;; where the filenames start matching.
+        suffixes  ;; replacement regexps for the matching regexp.
+        format    ;; what we have to match.
+        found     ;; name of the file or buffer found - nil if none.
+        stub      ;; name of file without extension.
+        (dirs '("../*")) ;; where to look for the file.
+        (alist '(("Tests\\.m\\'" (".m"))
+                 ("\\.m\\'" ("Tests.m")))) ;; candidate extensions.
+        (fname (file-name-nondirectory buffer-file-name)) ;; basename
+        )
+    (setq match (car alist))
+    ;; find the table entry corresponding to this file
+    (setq pos (ff-string-match (car match) fname))
+    (while (and match (if (and pos (>= pos 0)) nil (not pos)))
+      (setq alist (cdr alist))
+      (setq match (car alist))
+      (setq pos (ff-string-match (car match) fname)))
+    ;; otherwise, suffixes contains what we need
+    (setq suffixes (car (cdr match))
+          found nil)
+    ;; otherwise build our filename stub
+    (cond
+     ;; get around the problem that 0 and nil both mean false!
+     ((= pos 0)
+      (setq format "")
+      (setq stub "")
+      )
+
+     (t
+      (setq format (concat "\\(.+\\)" (car match)))
+      (string-match format fname)
+      (setq stub (substring fname (match-beginning 1) (match-end 1)))
+      ))
+
+    (setq found
+          (ff-get-file dirs
+                       stub
+                       suffixes
+                       in-other-window))))
