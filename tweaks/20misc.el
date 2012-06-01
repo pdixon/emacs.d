6eb3c328710186412bff175260c9f163d5c3c121
Phillip DIXON <phil@dixon.gen.nz>
2012-06-01 16:34:48 +1200

    Have ibuffer show human sizes.

--- tweaks/20misc.el
+++ tweaks/20misc.el
@@ -76,6 +76,26 @@
             (ibuffer-auto-mode 1)
             (ibuffer-switch-to-saved-filter-groups "default")))
 
+(eval-after-load 'ibuffer
+  '(progn
+     ;; Use human readable Size column instead of original one
+     (define-ibuffer-column size-h
+       (:name "Size" :inline t)
+       (cond
+        ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
+        ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
+        (t (format "%8d" (buffer-size)))))))
+
+;; Modify the default ibuffer-formats
+(setq ibuffer-formats
+      '((mark modified read-only " "
+              (name 18 18 :left :elide)
+              " "
+              (size-h 9 -1 :right)
+              " "
+              (mode 16 16 :left :elide)
+              " "
+              filename-and-process)))
 
 ;; Don't clutter up directories with files~
 (setq backup-directory-alist `(("." . ,(expand-file-name
