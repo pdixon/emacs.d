8c2af6fac15aa91d3f9d94a56bc965551e33026b
Phillip DIXON <phil@dixon.gen.nz>
2012-07-25 22:36:28 +1200

    gnus setup for mail and news.

--- dot-gnus.el
+++ dot-gnus.el
@@ -4,22 +4,25 @@
 (setq gnus-auto-subscribed-groups "^nnimap//")
 (setq gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
 
-;; I'm not using gnus as a news reader so I don't need any news stuff.
-(setq gnus-nntp-server nil
-      gnus-read-active-file nil
-      gnus-save-newsrc-file nil
-      gnus-read-newsrc-file nil
-      gnus-check-new-newsgroups nil)
+(setq gnus-select-method
+      '(nnimap "dixon.gen.nz"
+               (nnimap-address "imap.gmail.com")
+               (nnimap-server-port 993)
+               (nnimap-stream ssl)
+               (user "phil@dixon.gen.nz")
+               (nnir-search-engine imap)))
 
-(setq gnus-select-method '(nnimap "dixon.gen.nz"
-				  (nnimap-address "imap.gmail.com")
-				  (nnimap-server-port 993)
-				  (nnimap-stream ssl)
-                                  (user "phil@dixon.gen.nz")
-                                  (nnir-search-engine imap)))
+(add-to-list 'gnus-secondary-select-methods
+             '(nntp "news.gmane.org"
+                    (nntp-connection-timeout 360)
+                    (nntp-open-connection-function nntp-open-network-stream)
+                    (nntp-address "news.gmane.org")))
 
 ;; (setq imap-log t)
 (setq gnus-permanently-visible-groups "\.*")
+(setq gnus-topic-indent-level 3)
+(add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
+;;(setq gnus-group-line-format "%P|%B|%M%o%S%L[%6t|%3i]%6y :%(%~(pad-right 65)g%):%6,6~(cut 2)d\n")
 
 ;; Don't automatically make everything I've read expirable.
 (remove-hook 'gnus-mark-article-hook
@@ -29,16 +32,15 @@
 (gnus-add-configuration
  '(article
    (horizontal 1.0
-	       (vertical 30
-			 (group 1.0))
-	       (vertical 1.0
-			 (summary 0.25 point)
-			 (article 1.0)))))
+               (vertical 30
+                         (group 1.0))
+               (vertical 1.0
+                         (summary 0.25 point)
+                         (article 1.0)))))
 (gnus-add-configuration
  '(summary
    (horizontal 1.0
-	       (vertical 30
-			 (group 1.0))
-	       (vertical 1.0
-			 (summary 1.0 point)))))
-
+               (vertical 30
+                         (group 1.0))
+               (vertical 1.0
+                         (summary 1.0 point)))))
