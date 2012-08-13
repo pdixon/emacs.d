f4467e85a9ea236f5e616416f841fcfc0b5b9b56
Phillip DIXON <phil@dixon.gen.nz>
2012-08-13 22:32:43 +1200

    Load yasnippet on demand.

--- tweaks/40yasnippets.el
+++ tweaks/40yasnippets.el
@@ -1,6 +1,19 @@
 ;; Setup for yasnippets
 
-(require 'yasnippet)
-(yas/initialize)
-(setq yas/root-directory (concat dotfiles-dir "snippets"))
-(yas/load-directory yas/root-directory)
+(defmacro hook-into-modes (func modes)
+  `(dolist (mode-hook ,modes)
+     (add-hook mode-hook ,func)))
+
+(use-package yasnippet
+  :commands (yas/minor-mode yas/expand)
+  :mode ("/\\.emacs\\.d/snippets/" . snippet-mode)
+  :init
+  (hook-into-modes #'(lambda () (yas/minor-mode 1))
+                   '(prog-mode-hook
+                     org-mode-hook
+                     message-mode-hook
+                     markdown-mode-hook))
+  :config
+  (progn
+    (setq yas/root-directory (concat dotfiles-dir "snippets"))
+    (yas/load-directory yas/root-directory)))
