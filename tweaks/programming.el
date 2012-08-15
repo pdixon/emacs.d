2ae760dec9f36cf0dc1cf841b7709c3251a3fd82
Phillip DIXON <phil@dixon.gen.nz>
2012-08-15 22:48:20 +1200

    lua set up

--- tweaks/programming.el
+++ tweaks/programming.el
@@ -5,6 +5,11 @@
     (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
     (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)))
 
+(use-package lua
+  :mode ("\\.lua\\'" . lua-mode)
+  :interpreter (("lua" . lua-mode)
+                ("luajit" . lua-mode)))
+
 (use-package python
   :mode ("\\.py\\'" . python-mode)
   :interpreter ("python" . python-mode)
