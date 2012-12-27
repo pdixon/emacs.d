5776ee8186a7ba44d8a5a852c484bcce83437792
Phillip DIXON <phil@dixon.gen.nz>
2012-12-27 21:24:24 +1300

    Show linenums during goto-line.
    Sourced from 'what the .emacs.d'

--- tweaks/30defuns.el
+++ tweaks/30defuns.el
@@ -279,4 +279,15 @@
   (interactive)
   (say-text ""))
 
+(global-set-key [remap goto-line] 'goto-line-with-feedback)
+
+(defun goto-line-with-feedback ()
+  "Show line numbers temporarily, while prompting for the line number input"
+  (interactive)
+  (unwind-protect
+      (progn
+        (linum-mode 1)
+        (goto-line (read-number "Goto line: ")))
+    (linum-mode -1)))
+
 (provide '30defuns)
