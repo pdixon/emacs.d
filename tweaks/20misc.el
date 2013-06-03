f8dfa3c5cd6966d9bb29b841c4b4b4a152382c36
Phillip DIXON <phil@dixon.gen.nz>
2013-06-03 19:55:32 +1200

    Smart move to beginning of line.
    From emacs redux. C-a now moves to beginning of indentation then
    beginning of line.

--- tweaks/20misc.el
+++ tweaks/20misc.el
@@ -211,8 +211,34 @@
 (bind-key "M-j" 'delete-indentation)
 
 (bind-key "C-h a" 'apropos)
-(bind-key "C-h i" 'info-apropos)
+
 (bind-key "C-c y" 'bury-buffer)
+
+(defun smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+(global-set-key [remap move-beginning-of-line]
+                'smarter-move-beginning-of-line)
 
 (use-package bookmark
   :bind ("<f9>" . bookmark-bmenu-list))
