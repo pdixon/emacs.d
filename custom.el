1e390586c37ffcfacd082990645cc79945223caa
Phillip DIXON <phil@dixon.gen.nz>
2014-01-06 09:14:14 +1300

    don't check package sigs for now.

--- custom.el
+++ custom.el
@@ -4,11 +4,22 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ns-pop-up-frames nil)
+ '(package-check-signature nil)
  '(pr-gv-command "evince")
  '(pr-ps-printer-alist (quote ((default "gtklp" nil "" nil))))
  '(pr-txt-printer-alist (quote ((default "gtklp" nil nil))))
- '(safe-local-variable-values (quote ((emacs-lisp-docstring-fill-column . 75) (user-mail-address . "pdixon@dynamiccontrols.com") (user-company . "Dynamic Controls"))))
+ '(safe-local-variable-values
+   (quote
+    ((emacs-lisp-docstring-fill-column . 75)
+     (user-mail-address . "pdixon@dynamiccontrols.com")
+     (user-company . "Dynamic Controls"))))
  '(scroll-bar-mode nil)
  '(scss-compile-at-save nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
