357c5c26c5e8e057d10bb84497b3547e176ed374
Phillip DIXON <phil@dixon.gen.nz>
2014-01-26 20:15:22 +1300

    Make the height system dependent.

--- pd-basic-theme.el
+++ pd-basic-theme.el
@@ -1,8 +1,11 @@
 (deftheme pd-basic
   "Created 2014-01-13.")
 
-(custom-theme-set-faces
- 'pd-basic
- '(default ((t (:height 90 :family "Source Code Pro")))))
+(let ((height (if (eq system-type 'darwin)
+                  120
+                90)))
+  (custom-theme-set-faces
+   'pd-basic
+   `(default ((t (:height ,height :family "Source Code Pro"))))))
 
 (provide-theme 'pd-basic)
