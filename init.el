6d13a72205e748dca071e576f6438686861c3004
Phillip DIXON <phil@dixon.gen.nz>
2014-02-03 21:56:52 +1300

    More mucking about with ansi-term.

--- init.el
+++ init.el
@@ -203,13 +203,16 @@
         (switch-to-buffer "*ansi-term*"))))
   :config
   (progn
-    (defun my-term-use-utf8 ()
-      (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
-    (add-hook 'term-exec-hook 'my-term-use-utf8)
+    (defun my-term-paste (&optional string)
+      (interactive)
+      (process-send-string
+       (get-buffer-process (current-buffer))
+       (if string
+           string
+         (current-kill 0))))
 
     (defun my-term-hook ()
-      (goto-address-mode)
-      (define-key term-raw-map "\C-y" 'my-term-paste))
+      (goto-address-mode))
     (add-hook 'term-mode-hook 'my-term-hook)))
 
 (use-package 50org-mode)
