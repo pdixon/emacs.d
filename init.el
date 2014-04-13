2dfe25dda8e0cf2ebcc6fdadb88fdf7a86ca6172
Phillip DIXON <phil@dixon.gen.nz>
2014-04-13 20:51:24 +1200

    Don't use marmalade any more.
    The last package I was using from it was Auctex, and that's now in the
    gnu archive.

--- init.el
+++ init.el
@@ -70,7 +70,6 @@
   (unless (package-installed-p package)
     (package-install package)))
 
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 
 (package-initialize)
