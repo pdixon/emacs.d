d2a30f8c5e7f56d27bf367cd949f07d2c7d3f096
Phillip DIXON <phil@dixon.gen.nz>
2015-04-06 17:52:58 +1200

    Pinboard initial operational capability.
    Calling pinboard-list-bookmarks will now show a buffer with a list of
    all bookmarks.

--- lisp/pinboard.el
+++ lisp/pinboard.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2015  Phillip Dixon
 
 ;; Author: Phillip Dixon <phil@dixon.gen.nz>
-;; 
+;;
 
 ;; This file is NOT part of GNU Emacs
 
@@ -22,14 +22,15 @@
 
 ;;; Commentary:
 
-;; 
+;;
 
 ;;; Code:
 
 (require 'auth-source)
 (require 'cl-lib)
 (require 'json)
-(require 'seq)
+(require 'parse-time)
+(require 'subr-x)
 (require 'tabulated-list)
 (require 'url)
 
@@ -47,6 +48,9 @@
 (cl-defstruct pinboard-tag-detail
   name
   count)
+
+(defvar pinboard-bookmarks-last-fetched nil
+  "Last time all bookmarks were successfully fetched.")
 
 (defvar pinboard-bookmarks nil
   "List of pinboard-bookmark structs.")
@@ -85,60 +89,92 @@
            `((format json)
              ,@arguments))))
 
-(defun pinboard-request (method callback &optional arguments)
+(defun pinboard-request (method &optional arguments)
   ""
   (let* ((creds (pinboard-credentials))
          (creds-save-func (nth 2 creds))
          (url (pinboard-build-url method (nth 0 creds) (nth 1 creds) arguments)))
-    (url-retrieve
-     url
-     (lambda (_)
-       (goto-char (point-min))
-       (unless (string-match "200" (thing-at-point 'line))
-         (error "Page not found: %s" (thing-at-point 'line)))
-       (when (boundp 'url-http-end-of-headers)
-         (goto-char url-http-end-of-headers))
-       (funcall callback (let ((json-object-type 'plist)
-                               (json-array-type 'list)
-                               (json-key-type 'keyword))
-                           (json-read)))
-       (when creds-save-func
-         (funcall creds-save-func)))
-     nil t)))
+    (with-current-buffer (url-retrieve-synchronously url)
+      (goto-char (point-min))
+      (unless (string-match "200" (thing-at-point 'line))
+        (error "Page not found: %s" (thing-at-point 'line)))
+      (when (boundp 'url-http-end-of-headers)
+        (goto-char url-http-end-of-headers))
+      (when creds-save-func
+        (funcall creds-save-func))
+      (let ((json-object-type 'plist)
+            (json-array-type 'list)
+            (json-key-type 'keyword))
+        (json-read)))))
+
+(defun pinboard-request-posts-updated ()
+  ""
+  (when-let ((response (pinboard-request "posts/update")))
+    (let ((time-string (plist-get response :update_time)))
+      (parse-iso8601-time-string time-string))))
+
+(defun pinboard-request-posts-all ()
+  ""
+  (when-let ((response (pinboard-request "posts/all")))
+    (mapcar
+     (lambda (item)
+       (let ((time (parse-iso8601-time-string (plist-get item :time)))
+             (title (plist-get item :description))
+             (url (plist-get item :href))
+             (tags (split-string (plist-get item :tags))))
+         (make-pinboard-bookmark
+          :time time
+          :title title
+          :url url
+          :tags tags)))
+     response)))
 
 ;;; bookmark list mode stuff
 
 (defun pinboard-refresh-bookmarks ()
   ""
-  )
+  (when (or (not pinboard-bookmarks)
+            (not pinboard-bookmarks-last-fetched)
+            (time-less-p pinboard-bookmarks-last-fetched
+                         (pinboard-request-posts-updated)))
+    (when-let ((bookmarks (pinboard-request-posts-all)))
+      (setq pinboard-bookmarks bookmarks)
+      (setq pinboard-bookmarks-last-fetched (current-time)))))
 
 (defun pinboard-bookmark-list-entry (bookmark)
   ""
   (list
    (pinboard-bookmark-url bookmark)
    (vector
-    (pinboard-bookmark-time bookmark)
-    (pinboard-bookmark-tags bookmark)
+    (format-time-string "%F" (pinboard-bookmark-time bookmark))
+    (format "%s" (pinboard-bookmark-tags bookmark))
     (pinboard-bookmark-title bookmark))))
-
-(defun pinboard-bookmark-list-entries ()
-  ""
-  (seq-map #'pinboard-bookmark-list-entry pinboard-bookmarks))
 
 (define-derived-mode pinboard-bookmark-list-mode
     tabulated-list-mode
     "Pinboard-Bookmarks"
   "Major mode for viewing lists of pinboard.in bookmarks."
   (setq tabulated-list-format
-        '(("Time" 13 t)
-          ("Tags" 30 t)
-          ("Title" 46 t)))
-  (setq tabulated-list-entries #'pinboard-bookmark-list-entries)
+        (vector '("Time" 13 t)
+                '("Tags" 30 t)
+                '("Title" 46 t)))
+  (setq tabulated-list-entries
+        #'(lambda ()
+            (mapcar #'pinboard-bookmark-list-entry pinboard-bookmarks)))
   (add-hook 'tabulated-list-revert-hook #'pinboard-refresh-bookmarks nil t)
   (tabulated-list-init-header))
 
-;;; bookmark detail mode stuff
 
+(defun pinboard-list-bookmarks ()
+  (interactive)
+  (with-current-buffer (get-buffer-create "*pinboard*")
+    (let ((inhibit-read-only t))
+      (erase-buffer)
+      (pinboard-bookmark-list-mode))
+    (switch-to-buffer (current-buffer))
+    (revert-buffer)))
+
+;;; bookmark detail mode stuff
 
 ;;; tag list mode stuff
 
