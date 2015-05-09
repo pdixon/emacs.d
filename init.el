84813001b68629a8783fa141b9582efbf0912b55
Phillip DIXON <phil@dixon.gen.nz>
2015-05-09 18:07:53 +1200

    Arrange for code site-lisp to be seen as packages.
    This means that any thing that I have checked out and build directly is
    seen by package. Once package knows about it can satisfy other packages
    that depend on it.

--- init.el
+++ init.el
@@ -74,7 +74,7 @@
 (require 'package)
 
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-
+(add-to-list 'package-directory-list (concat dotfiles-dir "site-lisp"))
 (package-initialize)
 (setq package-enable-at-startup nil)
 (unless (package-installed-p 'use-package)
@@ -506,10 +506,9 @@
   :commands (pinboard-list-bookmarks))
 
 (use-package magit
-  ;:ensure t
-  :load-path "site-lisp/magit"
   :bind ("<f7>" . magit-status)
   :config
+  (setq magit-completing-read-function 'magit-ido-completing-read)
   (setq magit-status-buffer-switch-function 'switch-to-buffer))
 
 (use-package orgit
