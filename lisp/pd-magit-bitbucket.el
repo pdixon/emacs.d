aa410e7017952df8c9340bd16d386af892d6c3d7
Phillip Dixon <phil@dixon.gen.nz>
2015-11-15 22:22:37 +1300

    Another start on making pull requests quick.

---
+++ lisp/pd-magit-bitbucket.el
@@ -0,0 +1,44 @@
+;;; pd-magit-bitbucket.el --- Bitbucket helpers for magit  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  Phillip Dixon
+
+;; Author: Phillip Dixon <phil@dixon.gen.nz>
+;; Keywords: vc, tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(require 'magit-git)
+
+;;;#autoload
+(defun pd-magit-bb-visit-pull-request ()
+  "Visit the current branch's PR on bitbucket."
+  (interactive)
+  (message
+   (format "%s/pull-requests/new?source=%s"
+           (replace-regexp-in-string
+            "\\`\\(.+\\)\\.git\\'" "\\1"
+            (magit-get "remote"
+                       (magit-get-remote (magit-branch-at-point))
+                       "url"))
+           (cdr (or (magit-get-remote-branch (magit-branch-at-point))
+                    (user-error "No remote branch"))))))
+
+(provide 'pd-magit-bitbucket)
+;;; pd-magit-bitbucket.el ends here
