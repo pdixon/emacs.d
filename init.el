pdixon/emacs.d

commit f64975f7f5390d389ae497395e618cdfbcd8d35c

Let's start trying the tree sitter support

init.el
@@ -1447,8 +1447,11 @@ point reaches the beginning or end of the buffer, stop there."
   :commands (eglot eglot-ensure)
   :hook ((swift-mode . eglot-ensure)
          (rust-mode . eglot-ensure)
+         (rust-ts-mode . elgot-ensure)
          (c-mode . eglot-ensure)
          (c++-mode . eglot-ensure)
+         (c-ts-mode . eglot-ensure)
+         (c++-ts-mode . eglot-ensure)
          (obc-c-mode . eglot-ensure))
   :config
   (setq eglot-strict-mode nil)
@@ -1497,6 +1500,14 @@ point reaches the beginning or end of the buffer, stop there."
 (use-package age
   :ensure t
   :defer t)
+
+(use-package treesit
+  :defer t
+  :init
+  (setq treesit-language-source-alist
+        '((c . ("https://github.com/tree-sitter/tree-sitter-c"))
+          (cpp . ("https://github.com/tree-sitter/tree-sitter-cpp"))
+          (rust . ("https://github.com/tree-sitter/tree-sitter-rust")))))
 
 (defvar user-company "Phillip Dixon")
 

commit f43e6a9448ccf94b45f838ec795f3e704d737c24

Fix typo
Fixes: f64975f

init.el
@@ -1446,7 +1446,7 @@ point reaches the beginning or end of the buffer, stop there."
   :commands (eglot eglot-ensure)
   :hook ((swift-mode . eglot-ensure)
          (rust-mode . eglot-ensure)
-         (rust-ts-mode . elgot-ensure)
+         (rust-ts-mode . eglot-ensure)
          (c-mode . eglot-ensure)
          (c++-mode . eglot-ensure)
          (c-ts-mode . eglot-ensure)