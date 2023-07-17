53fc27b500904d80553d7fe794b516f846220b2e
Phillip Dixon <phil@dixon.gen.nz>
2023-07-17 16:33:07 +1200

    Updates to automatically managed files

--- custom.el
+++ custom.el
@@ -5,6 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(ns-pop-up-frames nil)
  '(ns-use-srgb-colorspace t)
+ '(package-vc-selected-packages
+   '((breadcrumb :url "https://github.com/joaotavora/breadcrumb.git")))
  '(safe-local-variable-values
    '((emacs-lisp-docstring-fill-column . 75)
      (user-mail-address . "pdixon@dynamiccontrols.com")
@@ -13,3 +15,9 @@
  '(use-package-enable-imenu-support t)
  '(use-package-ensure-function #'pd-package-ensure-elpa)
  '(warning-suppress-types '((comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
