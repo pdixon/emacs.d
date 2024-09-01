591c17e8920167a9d21b85c5a99b58300b451ec8
Phillip Dixon <phil@dixon.gen.nz>
2024-09-01 20:22:49 +1200

    git-commit is now part of magit
    It isn't a stand-alone package anymore.

--- init.el
+++ init.el
@@ -791,7 +791,9 @@ point reaches the beginning or end of the buffer, stop there."
   (setq magit-display-buffer-function #'magit-display-buffer-fullcolumn-most-v1)
   (setq magit-branch-prefer-remote-upstream '("master"))
   (setq magit-refs-pad-commit-counts t)
-  (setq magit-pull-or-fetch t))
+  (setq magit-pull-or-fetch t)
+  (add-to-list 'git-commit-trailers "Ticket")
+  (add-to-list 'git-commit-trailers "Fixes"))
 
 (use-package magit-repos
   :defer t
@@ -819,13 +821,6 @@ point reaches the beginning or end of the buffer, stop there."
 (use-package git-link
   :ensure t
   :defer t)
-
-(use-package git-commit
-  :ensure t
-  :defer t
-  :config
-  (add-to-list 'git-commit-trailers "Ticket")
-  (add-to-list 'git-commit-trailers "Fixes"))
 
 (use-package diff
   :config
