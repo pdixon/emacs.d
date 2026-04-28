b760f98a59809590c3a8ee228dab3b2ed1e2d9a9
Phillip Dixon <phil@dixon.gen.nz>
2026-04-28 16:36:31 +1200

    Check in my c++ template

---
+++ mytemplates/template.cpp
@@ -0,0 +1,7 @@
+//  `(file-name-nondirectory (buffer-file-name))`
+//
+//  Created by `(user-full-name)` on `(format-time-string "%Y-%02m-%d")`
+//  Copyright (c) `(format-time-string "%Y")` `(my-organisation)`. All rights reserved.
+//
+
+$0
